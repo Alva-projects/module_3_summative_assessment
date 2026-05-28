a351b2cd82e909ec7026de76b9340e0ec2b3804b
alva-projects <alva.lundberg@hotmail.se>
2026-05-28 12:28:02 +0200

    env variable allowed

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,8 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  const origin = process.env.CORS_ORIGIN || "*";
+  res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   if (req.method === "OPTIONS") return res.sendStatus(200);
