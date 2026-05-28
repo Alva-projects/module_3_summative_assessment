60eeb212c6e3ff6e832455b89665b78d4b46d7ed
alva-projects <alva.lundberg@hotmail.se>
2026-05-28 12:27:13 +0200

    firebaseAdmin

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -1,9 +1,15 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../../serviceAccountKey.json");
+if (!admin.apps.length) {
+  const credential = process.env.FIREBASE_PRIVATE_KEY
+    ? admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      })
+    : admin.credential.cert(require("../../serviceAccountKey.json"));
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  admin.initializeApp({ credential });
+}
 
 module.exports = admin;
